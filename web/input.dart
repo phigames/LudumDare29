15dc42f185cf795ced339ecc950b309475a4ecf8
Andreas Säuberli <aandivb@gmail.com>
2014-04-26 12:02:47 +0200

    Root Growth, Lakes, Water Supply

--- web/input.dart
+++ web/input.dart
@@ -1,11 +1,53 @@
 part of ld29;
 
+bool mouseDown = false;
+bool dragging = false;
+num addRootX1, addRootY1, addRootX2, addRootY2;
+Root addRoot;
+
 void onClick(MouseEvent event) {
-  num x = getXInWorld(event.layer.x);
-  num y = getYInWorld(event.layer.y);
-  Root r = mainRoot.getRootWhichHasPointAround(x, y);
-  if (r != null) {
-    r.addSubroot(x, y);
+  
+}
+
+void onMouseDown(MouseEvent event) {
+  if (event.button == 0) {
+    mouseDown = true;
+  }
+}
+
+void onMouseUp(MouseEvent event) {
+  if (event.button == 0) {
+    mouseDown = false;
+  }
+}
+
+void onMouseMove(MouseEvent event) {
+  if (mouseDown) {
+    num x = getXInWorld(event.layer.x);
+    num y = getYInWorld(event.layer.y);
+    if (!dragging) {
+      Root r = mainRoot.getRootWhichHasPointAround(x, y);
+      if (r != null) {
+        dragging = true;
+        addRootX1 = x;
+        addRootY1 = y;
+        addRootX2 = x;
+        addRootY2 = y;
+        addRoot = r;
+      }
+    } else {
+      addRootX2 = x;
+      addRootY2 = y;
+    }
+  } else if (dragging) {
+    dragging = false;
+    num dx = addRootX2 - addRootX1;
+    num dy = addRootY2 - addRootY1;
+    if (dy < 0) {
+      addRoot.addSubroot(new Root(addRootX1, addRootY1, atan(dx / dy) + PI, sqrt(dx * dx + dy * dy)));
+    } else {
+      addRoot.addSubroot(new Root(addRootX1, addRootY1, atan(dx / dy), sqrt(dx * dx + dy * dy)));
+    }
   }
 }
 
--- web/ld29.dart
+++ web/ld29.dart
@@ -9,13 +9,22 @@ CanvasRenderingContext2D canvas;
 CanvasRenderingContext2D buffer;
 int canvasWidth, canvasHeight;
 ImageElement background;
+ImageElement unknown;
+ImageElement water;
+ImageElement lava;
 num lastTime;
+Random random;
 num worldX, worldY, worldWidth, worldHeight;
 num worldScale;
 bool keyLeft, keyUp, keyRight, keyDown;
+List<Lake> lakes;
 Root mainRoot;
 num growTime;
 num growInterval;
+num waterTime;
+num waterInterval;
+num waterSupply;
+int waterLakes;
 
 void main() {
   CanvasElement c = querySelector('#canvas');
@@ -24,26 +33,42 @@ void main() {
   buffer = b.context2D;
   onResize(null);
   background = new ImageElement();
-  background.src = 'res/background.png';
+  //background.src = 'res/background.png';
+  unknown = new ImageElement();
+  unknown.src = 'res/unknown.png';
+  water = new ImageElement();
+  water.src = 'res/water.png';
+  lava = new ImageElement();
+  lava.src = 'res/lava.png';
   lastTime = -1;
-  worldX = 0;
+  random = new Random();
+  worldX = 1000 - canvasWidth / 2;
   worldY = 0;
   worldWidth = 2000;
-  worldHeight = 1500;
+  worldHeight = 10000;
   worldScale = 1.0;
   keyLeft = false;
   keyUp = false;
   keyRight = false;
   keyDown = false;
   c.onClick.listen(onClick);
+  c.onMouseDown.listen(onMouseDown);
+  c.onMouseUp.listen(onMouseUp);
+  c.onMouseMove.listen(onMouseMove);
   c.onMouseWheel.listen(onMouseWheel);
   document.onKeyDown.listen(onKeyDown);
   document.onKeyUp.listen(onKeyUp);
   window.onResize.listen(onResize);
   window.animationFrame.then(frame);
-  mainRoot = new Root(1000, 200, 0);
+  lakes = new List<Lake>();
+  lakes.add(new Lake(1000, 500));
+  mainRoot = new Root(1000, 200, 0, 120);
   growTime = 0;
   growInterval = 500;
+  waterTime = 0;
+  waterInterval = 500;
+  waterSupply = 105;
+  waterLakes = 0;
 }
 
 void frame(num time) {
@@ -51,7 +76,9 @@ void frame(num time) {
     update(time - lastTime);
   }
   lastTime = time;
+  buffer.clearRect(0, 0, canvasWidth, canvasHeight);
   drawWorld();
+  canvas.clearRect(0, 0, canvasWidth, canvasHeight);
   canvas.drawImage(buffer.canvas, 0, 0);
   window.animationFrame.then(frame);
 }
@@ -62,6 +89,11 @@ void update(num time) {
   if (growTime >= growInterval) {
     mainRoot.grow();
     growTime -= growInterval;
+  }
+  waterTime += time;
+  if (waterTime >= waterInterval) {
+    waterSupply += waterLakes;
+    waterTime -= waterInterval;
   }
 }
 
@@ -83,16 +115,16 @@ num getYInWorld(num y) {
 
 void moveWorld(num time) {
   if (keyLeft) {
-    worldX -= time / 2;
+    worldX -= time / 2 / worldScale;
   }
   if (keyUp) {
-    worldY -= time / 2;
+    worldY -= time / 2 / worldScale;
   }
   if (keyRight) {
-    worldX += time / 2;
+    worldX += time / 2 / worldScale;
   }
   if (keyDown) {
-    worldY += time / 2;
+    worldY += time / 2 / worldScale;
   }
   if (canvasWidth > worldWidth * worldScale) {
     worldScale = canvasWidth / worldWidth;
@@ -113,22 +145,38 @@ void moveWorld(num time) {
 }
 
 void drawWorld() {
-  buffer.drawImageToRect(background, new Rectangle<num>(0, 0, canvasWidth, canvasHeight), sourceRect: new Rectangle<num>(worldX, worldY, canvasWidth / worldScale, canvasHeight / worldScale));
+  //buffer.drawImageToRect(background, new Rectangle<num>(0, 0, canvasWidth, canvasHeight), sourceRect: new Rectangle<num>(worldX, worldY, canvasWidth / worldScale, canvasHeight / worldScale));
+  for (int i = 0; i < lakes.length; i++) {
+    lakes[i].draw();
+  }
   mainRoot.draw();
+  if (dragging) {
+    buffer.beginPath();
+    buffer.moveTo(getXOnCanvas(addRootX1), getYOnCanvas(addRootY1));
+    buffer.lineTo(getXOnCanvas(addRootX2), getYOnCanvas(addRootY2));
+    buffer.lineWidth = 2;
+    buffer.lineCap = 'round';
+    buffer.strokeStyle = '#008800';
+    buffer.stroke();
+  }
+  buffer.fillText(waterSupply.toString(), 0, 10);
 }
 
 class Root {
   
-  static const num POINT_DISTANCE = 30;
+  static const num POINT_DISTANCE = 20;
   
   List<Point<num>> points;
   num angle;
+  num length;
+  num target;
   List<Root> subroots;
   
-  Root(num x, num y, this.angle) {
+  Root(num x, num y, this.angle, this.target) {
     points = new List<Point<num>>();
     points.add(new Point<num>(x, y));
     points.add(new Point<num>(x + sin(angle) * POINT_DISTANCE, y + cos(angle) * POINT_DISTANCE));
+    length = POINT_DISTANCE;
     subroots = new List<Root>();
   }
   
@@ -139,18 +187,35 @@ class Root {
       }
     }
     for (int i = 0; i < subroots.length; i++) {
-      return subroots[i].getRootWhichHasPointAround(x, y);
+      Root s = subroots[i].getRootWhichHasPointAround(x, y);
+      if (s != null) {
+        return s;
+      }
     }
     return null;
   }
   
   void grow() {
-    Point l = points[points.length - 1];
-    points.add(new Point<num>(l.x + sin(angle) * POINT_DISTANCE, l.y + cos(angle) * POINT_DISTANCE));
+    if (length < target && waterSupply > 0) {
+      Point l = points[points.length - 1];
+      points.add(new Point<num>(l.x + sin(angle) * POINT_DISTANCE, l.y + cos(angle) * POINT_DISTANCE));
+      length += POINT_DISTANCE;
+      waterSupply--;
+      for (int i = 0; i < lakes.length; i++) {
+        if (lakes[i].contains(points[points.length - 1])) {
+          lakes[i].hit(this);
+        }
+      }
+    }
+    for (int i = 0; i < subroots.length; i++) {
+      subroots[i].grow();
+    }
   }
   
-  void addSubroot(num x, num y) {
-    subroots.add(new Root(x, y, 1));
+  void addSubroot(Root sub) {
+    if (waterSupply > 0) {
+      subroots.add(sub);
+    }
   }
   
   void draw() {
@@ -163,10 +228,65 @@ class Root {
       buffer.lineWidth = 20 * worldScale;
       buffer.lineCap = 'round';
       buffer.lineJoin = 'round';
+      buffer.strokeStyle = '#883300';
       buffer.stroke();
     }
     for (int i = 0; i < subroots.length; i++) {
       subroots[i].draw();
+    }
+  }
+  
+}
+
+class Lake {
+  
+  static const int WATER = 0;
+  static const int LAVA = 1;
+  
+  num x, y;
+  num width, height;
+  int type;
+  bool known;
+  
+  Lake(num x, num y) {
+    this.x = x - 75;
+    this.y = y - 50;
+    width = 150;
+    height = 100;
+    if (random.nextBool()) {
+      type = WATER;
+    } else {
+      type = LAVA;
+    }
+    known = false;
+  }
+  
+  bool contains(Point<num> point) {
+    if (point.x >= x && point.y >= y && point.x < x + width && point.y < y + height) {
+      return true;
+    }
+    return false;
+  }
+  
+  void hit(Root root) {
+    root.target = root.length;
+    if (!known) {
+      known = true;
+      if (type == WATER) {
+        waterLakes++;
+      }
+    }
+  }
+  
+  void draw() {
+    if (known) {
+      if (type == WATER) {
+        buffer.drawImageToRect(water, new Rectangle<num>(getXOnCanvas(x), getYOnCanvas(y), getXOnCanvas(width + worldX), getYOnCanvas(height + worldY)));
+      } else if (type == LAVA) {
+        buffer.drawImageToRect(lava, new Rectangle<num>(getXOnCanvas(x), getYOnCanvas(y), getXOnCanvas(width + worldX), getYOnCanvas(height + worldY)));
+      }
+    } else {
+      buffer.drawImageToRect(unknown, new Rectangle<num>(getXOnCanvas(x), getYOnCanvas(y), getXOnCanvas(width + worldX), getYOnCanvas(height + worldY)));
     }
   }
   
